69d2b4b4fb0ca0c06cce3821bee75b0dc2e3dfcc
johnwargo <john@johnwargo.com>
2025-01-20 09:21:00 -0500

    file input works

--- js/app.js
+++ js/app.js
@@ -1,7 +1,56 @@
+/*********************************************************
+ * Certificate to Arduino Converter
+ *********************************************************/
+const BUTTON_KEY = 'btnGenerate';
+const CERT_FILE_KEY = "certFile";
 const VARIABLE_NAME_KEY = "variableName";
 
 (function () {
   console.log("Initializing JavaScript library");
+  // Populate the form
   let arduinoVariable = localStorage.getItem(VARIABLE_NAME_KEY);
-  if (arduinoVariable) document.getElementById(VARIABLE_NAME_KEY).value = arduinoVariable;
+  document.getElementById(VARIABLE_NAME_KEY).value = arduinoVariable ? arduinoVariable : 'certfile';
+  // Add event listeners
+  document.getElementById(BUTTON_KEY).addEventListener('click', generateFile, false);
+  document.getElementById(CERT_FILE_KEY).addEventListener('cancel', fileSelectionCancelled, false);
+  document.getElementById(CERT_FILE_KEY).addEventListener('change', fileSelected, false);
+  document.getElementById(VARIABLE_NAME_KEY).addEventListener('change', saveVariableName, false);
 })();
+
+function saveVariableName(event) {
+  console.log(`Saving variable name: ${event.target.value}`);
+  localStorage.setItem(VARIABLE_NAME_KEY, event.target.value);
+}
+
+function fileSelectionCancelled(event) {
+  console.log(`Saving file selection cancelled`);
+}
+
+function fileSelected(event) {
+  console.log(`File selected`);
+  console.dir(event.target.files);
+
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function(event) {
+    document.getElementById('output').textContent = event.target.result;
+  };
+
+  reader.readAsText(file); // Read file as plain text
+}
+
+function generateFile(event) {
+  event.preventDefault();
+  console.log('Generating file');
+
+  const fileList = document.getElementById(CERT_FILE_KEY).files;
+  if (fileList.length > 0) {
+    console.dir(fileList);
+  } else {
+    console.log('No file found.');
+    alert('No file found.');
+  }
+
+
+}
