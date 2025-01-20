248dbae489804ea021a9c6b18d6f6ae454441cc5
johnwargo <john@johnwargo.com>
2025-01-20 10:55:59 -0500

    UI's working

--- js/app.js
+++ js/app.js
@@ -1,47 +1,75 @@
 /*********************************************************
  * Certificate to Arduino Converter
  *********************************************************/
+
+const DEBUG_MODE = false;
+
+const BLANK_STR = '';
 const BUTTON_KEY = 'btnGenerate';
 const CERT_FILE_KEY = "certFile";
+const OUTPUT_KEY = "output";
 const VARIABLE_NAME_KEY = "variableName";
 
+let certContent;
+
 (function () {
-  console.log("Initializing JavaScript library");
+  console.log("Initializing JavaScript code");
+  // Add event listeners
+  document.getElementById(VARIABLE_NAME_KEY).addEventListener('change', saveVariableName, false);
+  document.getElementById(VARIABLE_NAME_KEY).addEventListener('input', setButtonState, false);
+  document.getElementById(CERT_FILE_KEY).addEventListener('cancel', fileSelectionCancelled, false);
+  document.getElementById(CERT_FILE_KEY).addEventListener('change', fileSelected, false);
+  document.getElementById(CERT_FILE_KEY).addEventListener('change', setButtonState, false);
+  document.getElementById(BUTTON_KEY).addEventListener('click', generateFile, false);
+
   // Populate the form
   let arduinoVariable = localStorage.getItem(VARIABLE_NAME_KEY);
   document.getElementById(VARIABLE_NAME_KEY).value = arduinoVariable ? arduinoVariable : 'certfile';
-  // Add event listeners
-  document.getElementById(BUTTON_KEY).addEventListener('click', generateFile, false);
-  document.getElementById(CERT_FILE_KEY).addEventListener('cancel', fileSelectionCancelled, false);
-  document.getElementById(CERT_FILE_KEY).addEventListener('change', fileSelected, false);
-  document.getElementById(VARIABLE_NAME_KEY).addEventListener('change', saveVariableName, false);
+
+  certContent = BLANK_STR;
+  setButtonState();
 })();
 
+function setButtonState() {
+  const generateButton = document.getElementById(BUTTON_KEY);
+  const variableName = document.getElementById(VARIABLE_NAME_KEY).value;
+  if (DEBUG_MODE) {
+    console.log("Setting button state");
+    console.log(`Variable: "${variableName}"`);
+    console.log(`output: "${certContent}"`);
+  }
+  generateButton.disabled = (variableName.length < 1 || certContent.length < 1);
+}
+
 function saveVariableName(event) {
-  console.log(`Saving variable name: ${event.target.value}`);
+  console.log(`Saving variable name: "${event.target.value}"`);
   localStorage.setItem(VARIABLE_NAME_KEY, event.target.value);
 }
 
 function fileSelectionCancelled(event) {
-  console.log(`Saving file selection cancelled`);
+  console.log(`File selection cancelled`);
 }
 
 function fileSelected(event) {
-  console.log(`File selected`);
-
-  const outputField = document.getElementById('output');
+  console.log(`File selection change`);
+  const outputField = document.getElementById(OUTPUT_KEY);
   const file = event.target.files[0];
   if (file) {
-    console.dir(file);
+    if (DEBUG_MODE) console.dir(file);
     const reader = new FileReader();
-    reader.onload = function(event) {
-      outputField.textContent = event.target.result;
+    // create the event code that loads the file into memory
+    reader.onload = function (event) {
+      // console.dir(event.target.result);
+      certContent = event.target.result;
+      outputField.value = event.target.result;
+      setButtonState();
     };
     reader.readAsText(file); // Read file as plain text
   } else {
-    outputField.textContent = '';
+    certContent = BLANK_STR;
+    outputField.value = BLANK_STR;
+    setButtonState();
   }
-
 }
 
 function generateFile(event) {
@@ -49,10 +77,13 @@ function generateFile(event) {
   console.log('Generating file');
 
   const fileList = document.getElementById(CERT_FILE_KEY).files;
+  const outputField = document.getElementById(OUTPUT_KEY);
+
   if (fileList.length > 0) {
     console.dir(fileList);
+    console.log(outputField.value);
   } else {
     console.log('No file found.');
-    alert('No file found.');
+
   }
 }
